c44c20b426391317b74e0bc3338533eda1fc73a0
giovol <187798689+giovol@users.noreply.github.com>
2025-01-23 20:16:29 +0100

    Renamed Kernel.cs to Program.cs

---
+++ source/Core/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.System;
+
+namespace altroso.Core
+{
+    public class Program : Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            
+        }
+        
+        protected override void Run()
+        {
+
+        }
+    }
+}
