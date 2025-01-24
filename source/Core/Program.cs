461642f154c3b8cf8a266433e50192328fa16772
giovol <187798689+giovol@users.noreply.github.com>
2025-01-24 15:43:00 +0100

    Add Mirage Desktop

--- source/Core/Program.cs
+++ source/Core/Program.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 using Cosmos.System;
+using Mirage.DE;
+using PrismAPI.Graphics.Fonts;
+using PrismAPI.Hardware.GPU;
 
 namespace altroso.Core
 {
     public class Program : Kernel
     {
-
+        public static Display Screen;
         protected override void BeforeRun()
         {
-            
+            DesktopEnvironment.Start("altroso", "0.1");
         }
         
         protected override void Run()
